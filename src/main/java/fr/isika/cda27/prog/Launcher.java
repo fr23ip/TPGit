9b4acfe4f513bcc042aee9844f6e15b50464f130
Francisca Ilabaca <fr23ip@gmail.com>
2024-07-24 14:44:36 +0200

    modification gitworld

--- src/main/java/fr/isika/cda27/prog/Launcher.java
+++ src/main/java/fr/isika/cda27/prog/Launcher.java
@@ -3,7 +3,7 @@ package fr.isika.cda27.prog;
 public class Launcher {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Word");
+		System.out.println("Hello GitWorld.");
 
 	}
 
